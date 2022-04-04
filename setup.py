aeaab6799c6c2666d13df77486a90fd05f445d8e
unknown <fmfabio.0991@yahoo.com.br>
2022-04-04 14:03:53 -0300

    alterado setup.py e add icon

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import requests
 import pandas as pd
 import datetime
 
+
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
 build_exe_options = {"packages": 
@@ -18,18 +19,20 @@ build_exe_options = {"packages":
     'asyncio',
     'PySimpleGUI'
     ],
-    'includes':['tkinter']
+    'includes':['tkinter'],
+    'include_files': ['config.ini', 'icon.ico']
 }
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    
 
 setup(
     name = "CalcTax",
     version = "0.1",
     description = "Calculadora de importacao",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("app.py", base=base)]
+    executables = [Executable("app.py", base=base, icon='icon.ico')]
 )
